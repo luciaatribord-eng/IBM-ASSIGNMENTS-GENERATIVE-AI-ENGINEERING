c40fb9aef74c7d1c6fe5821a0c320affac06c282
luciaatribord-eng <lucia.atribord@gmail.com>
2026-07-14 10:47:37 +0200

    Add files via upload

---
+++ 4_b_packaging_test.py
@@ -0,0 +1,8 @@
+"""
+Emotion Detection Module
+This module provides emotion detection functionality using Watson AI.
+"""
+
+from .emotion_detector import emotion_detector
+
+__all__ = ['emotion_detector']
